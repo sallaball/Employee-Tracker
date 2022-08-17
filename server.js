dd071f4d2c01ed31220d719035f8ab130ab1fae3
sallaball <sallaball1206@gmail.com>
2022-08-17 16:07:02 +0300

    added middleware and PORT

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
 const express = require('express');
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3003;
 const app = express();
 const db = require('./db/connection');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
 // const apiRoutes = require('./routes/apiRoutes');
+
+//express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//use api routes
+// app.use('/api', apiRoutes);
+
+//Default response for any other request (Not found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+//start server after DB connection
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+});
